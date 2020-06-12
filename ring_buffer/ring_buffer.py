c44b4ac768f88435471de29fc24a152570586704
Anthony K <antonyk@users.noreply.github.com>
2020-06-12 12:11:46 -0400

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.next = 0
+
 
     def append(self, item):
-        pass
+        if item != None and self.capacity > 0:
+            if len(self.storage) == self.capacity:
+                self.storage[self.next] = item
+                self.next = self.get_next(self.next, self.capacity-1)
+                # self.next = (self.next + 1) % (self.capacity)
+            else:
+                self.storage.append(item)
+                # self.next = len(self.storage-1)
 
     def get(self):
-        pass
+        return self.storage
+
+    def get_next(self, current, ceiling):
+        return (int(current)+1) % ceiling
